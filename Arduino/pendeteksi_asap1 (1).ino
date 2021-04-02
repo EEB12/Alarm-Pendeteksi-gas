4d6fcae43be5e19f51d291a9cd3e91dd68a78a53
EEB12 <rayhan.taqara@yahoo.com>
2021-04-02 23:22:58 +0700

    fix arduino

--- Arduino/pendeteksi_asap1 (1).ino
+++ Arduino/pendeteksi_asap1 (1).ino
@@ -5,7 +5,7 @@ int redled=3;	//pin output untuk led merah
 int greenled=2;	// pin output untuk led hijau
 int buzzer=4;	// pin output untuk buzzer
 int sensor= A0;	// pin input untuk sensor
-int sensorThresh=380; //
+int Pembatas=380; // batasan untuk sensor
 void setup()
 {
 pinMode(redled,OUTPUT);		//mengatur mode led merah
@@ -15,18 +15,20 @@ pinMode(sensor,INPUT);		// mengatur mode sensor asap
 Serial.begin(9600);
 lcd.begin(16,2);
 }
+
+
 void loop()
 {
 int analogValue= analogRead(sensor);
 Serial.print(analogValue);
 Serial.print("\n");
-if(analogValue>sensorThresh) // jika value analog melebihi batasan 
+if(analogValue>Pembatas) // jika value analog melebihi batasan 
 {
   
 digitalWrite(redled,HIGH);	//menyalakan led merah
 digitalWrite(greenled,LOW); // mematikan led hijau
 tone(buzzer,1000,10000);	//membunyikan buzzer
-lcd.clear();				
+lcd.clear();				//
 lcd.setCursor(0,1);
 lcd.print("GAS TERDETEKSI");
 delay(1000);
